892d6add98fac3c31315bbee4fb9955c2f0d978c
Juanes <juanestevantj@gmail.com>
2026-01-31 22:28:51 -0500

    Capture of input value + previous history

--- src/GifsApp.tsx
+++ src/GifsApp.tsx
@@ -1,15 +1,26 @@
 import CustomHeader from "./shared/components/CustomHeader"
 import SearchBar from "./shared/components/SearchBar"
 import { mockGifs } from "./mock-data/gifs.mock"
-import PreviousSeaches from "./gifs/components/PreviousSeaches"
+import PreviousSeaches from "./gifs/components/PreviousSearches"
 import GifList from "./gifs/components/GifList"
+import { useState } from "react"
 
 const GifsApp = () => {
+    const [previousSearches, setpreviousSearches] = useState<string[]>([])
+
+    const addSearch = (search:string) => {
+        setpreviousSearches(prev => [...prev, search])
+    }
+
+    const handleTermClick = (term:string) => {
+        console.log(term)
+    }
+
     return (
         <>
             <CustomHeader title="Buscador de Gifs" description="Descubre y comparte el gif perfecto"/>
-            <SearchBar placeholder="Buscador"/>
-            <PreviousSeaches searches={['Goku', 'Saitama', 'Elden Ring']}/>
+            <SearchBar onSearch={addSearch} placeholder="Buscador"/>
+            <PreviousSeaches handleClickPrevious={handleTermClick} searches={previousSearches}/>
             <GifList gifs={mockGifs}/>
         </>
     )
--- src/gifs/components/PreviousSearches.tsx
+++ src/gifs/components/PreviousSearches.tsx
@@ -1,15 +1,17 @@
 interface Props {
     searches: string[]
+    handleClickPrevious: (valor:string) => void
 }
 
-const PreviousSeaches = ({ searches }: Props) => {
+const PreviousSeaches = ({ searches, handleClickPrevious }: Props) => {
+ 
     return (
         <div className="previous-searches">
             <h2>Busquedas previas</h2>
             <ul className="previous-searches-list">
                 {
                     searches.map((term) => (
-                        <li key={term}>{term}</li>
+                        <li onClick={() => handleClickPrevious(term)} key={term}>{term}</li>
                     ))
                 }
             </ul>
