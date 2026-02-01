f0823fe44baf91620f91b8892a689259b568bc8b
Juanes <juanestevantj@gmail.com>
2026-01-31 22:30:42 -0500

    function to capture values ​​with the button

--- src/shared/components/SearchBar.tsx
+++ src/shared/components/SearchBar.tsx
@@ -1,12 +1,35 @@
+import { useState } from "react"
+
 interface Props {
     placeholder?:string
+    onSearch: (value:string) => void
 }
 
-const SearchBar = ({placeholder='Buscar'}:Props) => {
+const SearchBar = ({placeholder='Buscar', onSearch}:Props) => {
+
+    const [value, setValue] = useState('')
+
+    const inputValue = (e:React.ChangeEvent<HTMLInputElement>) => {
+        setValue(e.target.value)
+    }
+
+    const manageKeyDown = (e:React.KeyboardEvent) => {
+        if(e.key === 'Enter') {
+            handleClickSearch()
+        }
+    }
+
+    const handleClickSearch = () => {
+        if (!value.trim()) return
+        console.log(value)
+        onSearch(value)
+        setValue('')
+    }
+
     return (
         <div className="search-container">
-            <input type="text" name="" id="" placeholder={placeholder} />
-            <button>Buscar</button>
+            <input type="text" name="" id="" onKeyDown={manageKeyDown} onChange={inputValue} value={value} placeholder={placeholder} />
+            <button onClick={handleClickSearch}>Buscar</button>
         </div>
     )
 }
